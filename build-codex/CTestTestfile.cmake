07b94d8a4cb24a5abf8e523a195d8cbfa82998ba
WARE10sai <WARE10sai@icloud.com>
2025-11-07 22:10:42 +0900

    delete build codex

--- build-codex/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ware10sai/dev/orteaf
-# Build directory: /home/ware10sai/dev/orteaf/build-codex
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("orteaf")
-subdirs("tests")
--- build-codex/tests/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ware10sai/dev/orteaf/tests
-# Build directory: /home/ware10sai/dev/orteaf/build-codex/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/home/ware10sai/dev/orteaf/build-codex/tests/orteaf_tests[1]_include.cmake")
