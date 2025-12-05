3901bbabdd15ca094e0d0861990ac8c9cd080532
WARE10sai <WARE10sai@icloud.com>
2025-12-05 16:06:17 +0900

    feat:initalize on mps runtime public ops

--- orteaf/include/orteaf/internal/runtime/ops/mps/public/mps_public_ops.h
+++ orteaf/include/orteaf/internal/runtime/ops/mps/public/mps_public_ops.h
@@ -2,6 +2,11 @@
 
 #if ORTEAF_ENABLE_MPS
 
+#include <memory>
+
+#include "orteaf/internal/backend/mps/mps_slow_ops.h"
+#include "orteaf/internal/runtime/manager/mps/mps_runtime_manager.h"
+
 namespace orteaf::internal::runtime::ops::mps {
 
 class MpsPublicOps {
@@ -12,9 +17,19 @@ public:
     MpsPublicOps(MpsPublicOps&&) = default;
     MpsPublicOps& operator=(MpsPublicOps&&) = default;
     ~MpsPublicOps() = default;
+
+    using SlowOps = ::orteaf::internal::runtime::backend_ops::mps::MpsSlowOps;
+
+    // Initialize runtime with a provided SlowOps implementation, or default MpsSlowOpsImpl.
+    void initialize(std::unique_ptr<SlowOps> slow_ops = nullptr) {
+        ::orteaf::internal::runtime::mps::GetMpsRuntimeManager().initialize(std::move(slow_ops));
+    }
+
+    void shutdown() {
+        ::orteaf::internal::runtime::mps::GetMpsRuntimeManager().shutdown();
+    }
 };
 
 }  // namespace orteaf::internal::runtime::ops::mps
 
 #endif  // ORTEAF_ENABLE_MPS
-
